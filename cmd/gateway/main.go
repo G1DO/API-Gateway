5abf7bb51a5dd56d3d8c643fb44b965d71077546
Ibrahim <ibrahimmohamedabdelsadek@gmail.com>
2026-01-30 01:37:02 +0200

    Wire up gateway entrypoint and rename proxy http field to client
    Implement main() to start the reverse proxy server on :9000
    forwarding to localhost:8080. Rename proxy struct field from
    http to client to avoid shadowing the net/http package.

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+    "net/http"
+    "github.com/G1D0/Api-Gateway/internal/proxy"
+	"log"
+)
+
+func main() {
+    // 1. Create proxy: p := proxy.NewProxy("http://localhost:8080")
+	p := proxy.NewProxy("http://localhost:8080")
+    // 2. Start server: http.ListenAndServe(":9000", p)
+	log.Println("Proxy listening on :9000")
+    err := http.ListenAndServe(":9000", p)
+    if err != nil {
+    log.Fatal(err)
+    }
+	
+
+}
